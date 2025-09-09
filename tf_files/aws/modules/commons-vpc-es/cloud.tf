2cdaf81c117e80a538ce3d025d6ca229bc66d1c4
dongbohu <dongbo.hu@gmail.com>
2025-09-09 14:35:59 -0400

    update

--- tf_files/aws/modules/commons-vpc-es/cloud.tf
+++ tf_files/aws/modules/commons-vpc-es/cloud.tf
@@ -90,9 +90,8 @@ resource "aws_elasticsearch_domain" "gen3_metadata" {
   }
 
   advanced_options {
-    "rest.action.multi.allow_explicit_index" = "true"
+    rest.action.multi.allow_explicit_index = "true"
   }
-  depends_on = ["aws_iam_service_linked_role.es"]
 
   snapshot_options {
     automated_snapshot_start_hour = 23
